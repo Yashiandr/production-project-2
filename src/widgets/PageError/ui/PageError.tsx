515673edf58bca7e8ce7932211a26bdab84f2fbc
Nikita Andryashin <yashiandr@gmail.com>
2024-06-23 23:49:16 +0300

    fix PageError

--- src/widgets/PageError/ui/PageError.tsx
+++ src/widgets/PageError/ui/PageError.tsx
@@ -2,6 +2,7 @@ import { useTheme } from 'app/providers/ThemeProvider';
 import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Button } from 'shared/ui/Button/Button';
+import { Page } from '../../Page';
 import * as cls from './PageError.module.scss';
 
 interface PageErrorProps {
@@ -18,11 +19,11 @@ export const PageError = ({ className }: PageErrorProps) => {
         location.reload();
     };
     return (
-        <div className={classNames(cls.PageError, {}, [className, 'app', theme])}>
+        <Page className={classNames(cls.PageError, {}, [className, 'app', theme])}>
             <p>{t('Произошла непредвиденная ошибка')}</p>
             <Button onClick={reloadPage}>
                 {t('Обновить страницу')}
             </Button>
-        </div>
+        </Page>
     );
 };
